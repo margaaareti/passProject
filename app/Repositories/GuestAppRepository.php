12d7fd597c7b2df583771f0c51a6ac3b8b7f4f72
margaaareti <vladislav-an0@rambler.ru>
2023-04-29 23:59:06 +0300

    increment is work

--- app/Repositories/GuestAppRepository.php
+++ app/Repositories/GuestAppRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories;
 
+
+use App\Models\Counter;
 use App\Models\Guest;
 use App\Models\PeopleList;
 use Illuminate\Support\Facades\Auth;
@@ -12,77 +14,75 @@ class GuestAppRepository
 {
 
     protected PeopleList $appModel;
-    protected Guest $guestModel;
 
-    private static $lastDate;
-    private static int $count = 1;
 
-    public function __construct(PeopleList $appModel, Guest $guestModel)
+    public function __construct(PeopleList $appModel)
     {
         $this->appModel = $appModel;
-        $this->guestModel = $guestModel;
+
     }
+
 
     public function create(array $data)
     {
 
         $date = date('d.m.Y');
         $data['user_id'] = Auth::id();
-        $data['type']='проход';
+        $data['type'] = 'проход';
         $data['responsible_person'] = Auth::user()->name;
 
-        //получаем последнюю запись из базы данных
-        $lastRecord = $this->appModel->latest()->first();
 
-        //Проверяем, если дата последней записи совпадает с текущей датой
-        if ($lastRecord && $lastRecord->created_at->format('d.m.Y')==$date) {
-            //увеличиваем номер заявки на 1
-            self::$count += 1;
-        } else {
-            self::$count = 1;
+        for ($i = 0; $i < 10; $i++) {
+            //получаем последнюю запись из базы данных
+            $lastRecord = $this->appModel->latest()->first();
+
+
+            //Получаем значение счетчика из базы данных
+            $counter = Counter::find(1);
+            if (!$counter) {
+                $counter = new Counter(['value' => 1]);
+                $counter->save();
+            }
+
+            if ($lastRecord && $lastRecord->created_at->format('d.m.Y') == $date) {
+                $counter->increment('value');
+            } else {
+                $counter->update(['value'=>1]);
+                $counter->save();
+            }
+
+
+
+            // форматируем номер заявки в строку с нулями в начале
+            $number = sprintf('%03d', $counter->value);
+            $data['number'] = $date . '/' . $number;
+
+
+            $appList = $this->appModel->create($data);
+
+            foreach ($data['guests'] as $guest_name) {
+
+                $guest = new Guest(['name' => $guest_name]);
+
+                $guest->save();
+
+                $appList->guests()->attach($guest->id);
+            }
+
+
+            $array = [
+                $data['number'], $data['department'],//null,$data['signed_by'],$data['start_date'],$data['end_date'],
+                //null,$data['object'],$data['type'],
+                //null,$data['contract_number'],null,$data['equipment'],$data['guests'],null,null,null,null,
+                //null,$data['responsible_person'],$data['phone_number']
+            ];
+
+            $range = 'A2';
+
+
+            Sheets::spreadsheet(config('google.post_spreadsheet_id'))->sheetById('google.post_sheet_id')->range($range)->append([$array]);
+
         }
-
-
-
-        // форматируем номер заявки в строку с нулями в начале
-        $number = sprintf('%03d', self::$count);
-        $data['number']= $date . '/' . $number;
-
-
-
-        $appList = $this->appModel->create($data);
-
-        foreach ($data['guests'] as $guest_name) {
-
-            $guest = new Guest(['name'=> $guest_name]);
-
-            $guest->save();
-
-            $appList->guests()->attach($guest->id);
-        }
-
-
-
-
-
-        $array=[
-            $data['number'],$data['department'],//null,$data['signed_by'],$data['start_date'],$data['end_date'],
-            //null,$data['object'],$data['type'],
-            //null,$data['contract_number'],null,$data['equipment'],$data['guests'],null,null,null,null,
-            //null,$data['responsible_person'],$data['phone_number']
-        ];
-
-
-        $range = 'A2';
-
-
-
-        Sheets::spreadsheet(config('google.post_spreadsheet_id'))->sheetById('google.post_sheet_id')->range($range)->append([$array]);
-
-
-
-        return $appList;
-
     }
 
 }
