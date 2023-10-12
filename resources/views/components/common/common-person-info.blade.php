eb7bce58d17f43d8d03bfe90fd9bacfe9134a7bd
margaaareti <vladislav-an0@rambler.ru>
2023-10-12 09:06:52 +0300

    FUCK WITH CHECKBOX

--- resources/views/components/common/common-person-info.blade.php
+++ resources/views/components/common/common-person-info.blade.php
@@ -49,7 +49,7 @@
             </div>
         </div>
 
-        <x-input name="phone_number"
+        <x-input name="phone_number"git
                  class="number-field @error('phone_number') is-invalid @enderror"
                  value="{{ old('phone_number')}}"
         />
