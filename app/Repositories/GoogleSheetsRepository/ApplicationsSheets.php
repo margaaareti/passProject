b87c2d23733be29663d2192d4e65facfc328a713
margaaareti <vladislav-an0@rambler.ru>
2023-12-18 21:31:12 +0300

    add in ghoogle sheets but dont color

--- app/Repositories/GoogleSheetsRepository/ApplicationsSheets.php
+++ app/Repositories/GoogleSheetsRepository/ApplicationsSheets.php
@@ -28,25 +28,24 @@ class ApplicationsSheets
     public function addNewRecordToSheet(array $data)
     {
 
-        if(isset($data['cars'])){
+        if (isset($data['cars'])) {
             $data['guests'] = '';
             $data['cars'] = implode("\n", $data['cars']);
-            $data['application_type']= 'Въезд';
-            if(str_starts_with($data['object'], 'Ломоносова,9')){
+            $data['application_type'] = 'Въезд';
+            if (str_starts_with($data['object'], 'Ломоносова,9')) {
                 $data['object'] = 'Л9';
             }
         } elseif (isset($data['guests'])) {
             $data['cars'] = '';
             $data['guests'] = implode("\n", $data['guests']);
-            $data['application_type']= 'Проход';
+            $data['application_type'] = 'Проход';
         };
-
 
 
         $array = [
             $data['application_number'], $data['department'], '', $data['signed_by'], $data['start_date'], $data['end_date'],
             $data['time_range'], $data['object'], $data['application_type'], $data['purpose'],
-            $data['contract_number'], '', $data['equipment'], $data['guests'],$data['cars'], '', '', $data['responsible_person'], $data['phone_number']
+            $data['contract_number'], '', $data['equipment'], $data['guests'], $data['cars'], '', '', $data['responsible_person'], $data['phone_number']
         ];
 
 
@@ -63,64 +62,55 @@ class ApplicationsSheets
             $newSheet->range($range_to_fill)->append([$array]);
 
 
+            $range_to_paint = 'A:Z';
+            $spreadsheetId = '1aij-vtxBkhL7OpZjPutJdlThGZd0fdYVR7vtPidpVaA';
+
+
+            $response = Sheets::spreadsheet($spreadsheetId)->range($range_to_paint)->get();
+            $rows = $response->toArray();
+            $numRows = count($rows);
+
+            $startRow = $numRows - 2;
+
+            $service = Sheets::spreadsheet($spreadsheetId)->getService();
+
+
+            $requests = [
+                'updateCells' => [
+                    'rows' => [
+                        [
+                            'values' => [
+                                [
+                                    'userEnteredFormat' => [
+                                        'backgroundColor' => [
+                                            'red' => 1.0,
+                                            'green' => 1.0,
+                                            'blue' => 0.0
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ],
+                    'fields' => 'userEnteredFormat.backgroundColor',
+                    'start' => [
+                        'sheetId' => 0,
+                        'rowIndex' => $startRow,
+                        'columnIndex' => 0
+                    ]
+                ]
+            ];
+
+            $batchUpdateRequest = new \Google_Service_Sheets_BatchUpdateSpreadsheetRequest([
+                'requests' => $requests
+            ]);
+
+            $response = $service->spreadsheets->batchUpdate($spreadsheetId, $batchUpdateRequest);
+
         } catch (\Exception $e) {
             Log::error('Error sending data To google sheets: ' . $e->getMessage());
             return $e->getMessage();
         }
     }
-
-
-
-//        $range_to_fill = 'A2';
-//        $range_to_paint = 'A:Z';
-//        $spreadsheetId = '1QSGJj-_sfHAvJcFnPWLOsTDb71Wh_5DmEmpeuPW7iWg';
-//
-//
-//        $sheet = Sheets::spreadsheet(config('google.post_spreadsheet_id'))->sheetById('google.post_sheet_id')->range($range_to_fill)->append([$array]);
-//
-//
-//        $response = Sheets::spreadsheet($spreadsheetId)->range($range_to_paint)->get();
-//        $rows = $response->toArray();
-//        $numRows = count($rows);
-//
-//
-//        $startRow = $numRows - 1;
-//
-//        $service = Sheets::spreadsheet($spreadsheetId)->getService();
-//
-//
-//        $requests = [
-//            'updateCells' => [
-//                'rows' => [
-//                    [
-//                        'values' => [
-//                            [
-//                                'userEnteredFormat' => [
-//                                    'backgroundColor' => [
-//                                        'red' => 1.0,
-//                                        'green' => 1.0,
-//                                        'blue' => 0.0
-//                                    ]
-//                                ]
-//                            ]
-//                        ]
-//                    ]
-//                ],
-//                'fields' => 'userEnteredFormat.backgroundColor',
-//                'start' => [
-//                    'sheetId' => 0,
-//                    'rowIndex' => $startRow,
-//                    'columnIndex' => 0
-//                ]
-//            ]
-//        ];
-//
-//        $batchUpdateRequest = new \Google_Service_Sheets_BatchUpdateSpreadsheetRequest([
-//            'requests' => $requests
-//        ]);
-//
-//        $response = $service->spreadsheets->batchUpdate($spreadsheetId, $batchUpdateRequest);
-
-   // }
 
 }
